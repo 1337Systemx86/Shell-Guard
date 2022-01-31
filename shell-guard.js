3f4a8b39b292ec242a5b078a414a2d9b34fe1c3b
1337Systemx86 <98297861+1337Systemx86@users.noreply.github.com>
2022-01-31 18:19:36 +0100

    Add files via upload

---
+++ shell-guard.js
@@ -0,0 +1,20 @@
+//<![CDATA[
+    (function(){
+        var a = function() {try{return !!window.addEventListener} catch(e) {return !1} },
+        b = function(b, c) {a() ? document.addEventListener("DOMContentLoaded", b, c) : document.attachEvent("onreadystatechange", b)};
+        b(function(){
+        var a = document.getElementById('cf-content');a.style.display = 'block';
+        setTimeout(function(){
+        var s,t,o,p,b,r,e,a,k,i,n,g,f, baXzpvk={"VBz":+((!+[]+!![]+[])+(!+[]+!![]+!![]+!![]))};
+        t = document.createElement('div');
+        t.innerHTML="<a href='/'>x</a>";
+        t = t.firstChild.href;r = t.match(/https?:\/\//)[0];
+        t = t.substr(r.length); t = t.substr(0,t.length-1);
+        a = document.getElementById('jschl-answer');
+        f = document.getElementById('challenge-form');
+        ;baXzpvk.VBz*=+((!+[]+!![]+!![]+!![]+[])+(!+[]+!![]+!![]+!![]+!![]+!![]+!![]+!![]));baXzpvk.VBz-=+((!+[]+!![]+!![]+!![]+!![]+[])+(+[]));baXzpvk.VBz+=+((!+[]+!![]+[])+(+[]));baXzpvk.VBz-=+((!+[]+!![]+!![]+!![]+[])+(+!![]));baXzpvk.VBz+=!+[]+!![]+!![]+!![]+!![]+!![]+!![];baXzpvk.VBz*=+((!+[]+!![]+[])+(+!![]));baXzpvk.VBz-=+((!+[]+!![]+[])+(!+[]+!![]+!![]+!![]+!![]+!![]+!![]+!![]));baXzpvk.VBz+=+((!+[]+!![]+!![]+!![]+[])+(!+[]+!![]+!![]+!![]+!![]+!![]+!![]+!![]+!![]));a.value = parseInt(baXzpvk.VBz, 10) + t.length; '; 121'
+        f.submit();
+        }, 4000);
+        }, false);
+        })();
+        //]]>
